592f5d8c33aba30c32da9b94204b1cc36c0b18c5
James Halliday <mail@substack.net>
2013-06-03 04:36:57 -0700

    failing error tests

--- test/dir.js
+++ test/dir.js
@@ -2,7 +2,7 @@ var test = require('tap').test;
 var article = require('../')(__dirname + '/articles');
 var concat = require('concat-stream');
 
-test(function (t) {
+test('read articles', function (t) {
     t.plan(2);
     
     article('robot').pipe(concat(function (body) {
@@ -12,4 +12,11 @@ test(function (t) {
     article('turtle').pipe(concat(function (body) {
         t.equal(body, '<h1>turtles</h1>\n<p>I like turtles.</p>\n');
     }));
+});
+
+test('article exceptions', function (t) {
+    t.plan(2);
+    
+    article('../hax').on('error', function (err) { t.ok(err) });
+    article('no_such_file').on('error', function (err) { t.ok(err) });
 });
